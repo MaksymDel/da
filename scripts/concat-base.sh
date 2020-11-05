e7a8d2b00cdbdb9204344b9b5fe5e84610b5f9a8
Maksym Del <max.del.edu@gmail.com>
2020-11-05 20:15:44 +0200

    Clean up, new training command

--- scripts/concat-base.sh
+++ scripts/concat-base.sh
@@ -1,12 +1,15 @@
-SAVE_DIR="experiments/concat"
+EXP_NAME="concat_base"
+SAVE_DIR="experiments/"$EXP_NAME
 
 CUDA_VISIBLE_DEVICES=1 fairseq-train \
     data-prep/bin-data-en-et-base \
     --arch transformer --share-decoder-input-output-embed \
     --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
     --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
-    --dropout 0.3 --weight-decay 0.0001 \
+    --dropout 0.3 --weight-decay 0.0 \
     --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
     --max-tokens 4096 \
+    --log-format json \
     --save-dir $SAVE_DIR \
     --tensorboard-logdir $SAVE_DIR/"log-tb" \
+    2>&1 | tee $SAVE_DIR/"log.out"
--- scripts/concat-pilot.sh
+++
@@ -1,8 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 fairseq-train \
-    data-prep/bin-data-en-et-base \
-    --arch transformer_iwslt_de_en --share-decoder-input-output-embed \
-    --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0.0 \
-    --lr 5e-4 --lr-scheduler inverse_sqrt --warmup-updates 4000 \
-    --dropout 0.3 --weight-decay 0.0001 \
-    --criterion label_smoothed_cross_entropy --label-smoothing 0.1 \
-    --max-tokens 4096
